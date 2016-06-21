82edaa2dcdca2460eb0f5a9e0677887fd658e7b4
Jack Zhang <15jzhang@gmail.com>
2016-06-21 12:31:20 -0700

    Add dotenv environment variable configuration

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 require('babel-polyfill');
-const express = require('express');
 
+// Load environment variables
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: './env/development.env' });
+} else if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: './env/production.env' });
+}
+
+const express = require('express');
 const app = express();
-const PORT = 4040;
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Gobble template is listening on port ${PORT}.`);
+app.listen(process.env.PORT, () => {
+  console.log(`${process.env.APP_NAME} is listening on port ${process.env.PORT}.`);
 });
 
 module.exports = app;
