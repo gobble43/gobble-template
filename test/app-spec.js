04c687ac40be07a1350db94266c4360b254631e4
Jack Zhang <15jzhang@gmail.com>
2016-06-21 15:17:19 -0700

    Edit wording in app-spec

--- test/app-spec.js
+++ test/app-spec.js
@@ -1,6 +1,9 @@
 require('./setup');
 // const expect = require('chai').expect;
 
+// We're using supertest, which allows for use of any super-agent methods
+// and really easy HTTP assertions.
+// See https://www.npmjs.com/package/supertest for a better reference
 const appUrl = process.env.PROTOCOL + process.env.HOST + ':' + process.env.PORT;
 const request = require('supertest');
 
@@ -18,5 +21,5 @@ describe('Gobble Template', () => {
     });
   });
 
-  // More tests (as in A LOT more!) and describe statements below
+  // More tests (as in A LOT more!) and describe blocks below
 });
