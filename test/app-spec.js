30b59c3468a3c9f64ae7fb302cffbffbf014e17e
Jack Zhang <15jzhang@gmail.com>
2016-06-21 19:34:19 -0700

    Sample changes for demo

--- test/app-spec.js
+++ test/app-spec.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 // We're using supertest, which allows for use of any super-agent methods
 // and really easy HTTP assertions.
 // See https://www.npmjs.com/package/supertest for a better reference
-const appUrl = process.env.PROTOCOL + process.env.HOST + ':' + process.env.PORT;
+const appUrl = `${process.env.PROTOCOL}${process.env.HOST}:${process.env.PORT}`;
 const request = require('supertest');
 
 describe('Gobble Template', () => {
